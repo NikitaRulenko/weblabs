d8d9d614009f3431962b9776de665095bd2204bb
Irnil <32452526+Irnil@users.noreply.github.com>
2018-11-26 15:35:44 +0300

    Однообразные кавычки

--- practES6/src/menu.js
+++ practES6/src/menu.js
@@ -9,10 +9,10 @@ class Menu {
   }
 
   render() {
-    this.elem = document.createElement('div');
+    this.elem = document.createElement("div");
     this.elem.className = "menu";
 
-    const titleElem = document.createElement('span');
+    const titleElem = document.createElement("span");
     this.elem.appendChild(titleElem);
     titleElem.className = "title";
     titleElem.textContent = this.options.title;
@@ -20,7 +20,7 @@ class Menu {
     this.elem.onmousedown = () => false;
 
     this.elem.onclick = event => {
-      if (event.target.closest('.title')) {
+      if (event.target.closest(".title")) {
         this.toggle();
       }
     }
@@ -28,9 +28,9 @@ class Menu {
 
   renderItems() {
     const items = this.options.items || [];
-    const list = document.createElement('ul');
+    const list = document.createElement("ul");
     items.forEach(item => {
-      const li = document.createElement('li');
+      const li = document.createElement("li");
       li.textContent = item;
       list.appendChild(li);
     });
@@ -38,18 +38,18 @@ class Menu {
   }
 
   open() {
-    if (!this.elem.querySelector('ul')) {
+    if (!this.elem.querySelector("ul")) {
       this.renderItems();
     }
-    this.elem.classList.add('open');
+    this.elem.classList.add("open");
   };
 
   close() {
-    this.elem.classList.remove('open');
+    this.elem.classList.remove("open");
   };
 
   toggle() {
-    if (this.elem.classList.contains('open')) this.close();
+    if (this.elem.classList.contains("open")) this.close();
     else this.open();
   };
 }
