f1732e695dd41ebc41e4eb1de86210e00e09c5b4
Irnil <32452526+Irnil@users.noreply.github.com>
2019-06-03 22:58:17 +0300

    Mocha test added and beautified

--- lw9/tests.js
+++ lw9/tests.js
@@ -1,14 +1,14 @@
-describe("RecoveryPassword", function() {
+describe("RecoveryPassword", function () {
 
-    it("SMS recovery method", function() {
-      assert.equal(resultSMS, true);
-    });
-
-    it("Email recovery method", function() {
-        assert.equal(resultEmail, true);
-    });
-
-    it("Validate code method", function() {
-        assert.equal(resultValidate, false);
-    });
+  it("SMS recovery method", function () {
+    assert.equal(resultSMS, true);
   });
+
+  it("Email recovery method", function () {
+    assert.equal(resultEmail, true);
+  });
+
+  it("Validate code method", function () {
+    assert.equal(resultValidate, false);
+  });
+});
