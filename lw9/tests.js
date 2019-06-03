34fb9efeb4e870f241168a65b8a791b247fa83af
Irnil <32452526+Irnil@users.noreply.github.com>
2019-06-03 22:57:33 +0300

    Mocha test added

--- lw9/tests.js
+++ lw9/tests.js
@@ -1,95 +1,14 @@
-var autoTest = {
-    passedTestCount: 0,
-    code: 0,
-    codeGen: false,
-    RecoveryPassword_sms: false,
-    RecoveryPassword_email: false,
-    RecoveryPassword_validate: false,
-    emailSend: false,
-    smsSend: false,
+describe("RecoveryPassword", function() {
 
-    test_codeGen_generate() {
-        this.code = codeGenerator();
-        if (typeof (this.code) == "number" ||
-            toString(this.code).length == 5) {
-            this.codeGen = true;
-            this.passedTestCount++;
-            console.log("codeGen_test => PASSED");
-        }
-    },
+    it("SMS recovery method", function() {
+      assert.equal(resultSMS, true);
+    });
 
-    test_smsSend() {
-        Sms.send(this.code);
-        if (typeof (this.code) == "number" ||
-            toString(this.code).length == 5) {
-            this.smsSend = true;
-            this.passedTestCount++;
-            console.log("smsSend_test => PASSED");
-        }
-    },
+    it("Email recovery method", function() {
+        assert.equal(resultEmail, true);
+    });
 
-    test_emailSend() {
-        Email.send(this.code);
-        if (typeof (this.code) == "number" ||
-            toString(this.code).length == 5) {
-            this.emailSend = true;
-            this.passedTestCount++;
-            console.log("emailSend_test => PASSED");
-        }
-    },
-
-    test_RecoveryPassword_sms() {
-        code = RecoveryPassword.recovery("sms", "89877183558");
-        if (typeof (this.code) == "number" ||
-            toString(this.code).length == 5) {
-            this.RecoveryPassword_sms = true;
-            this.passedTestCount++;
-            console.log("RecoveryPassword_sms_test => PASSED");
-        }
-    },
-
-    test_RecoveryPassword_email() {
-        code = RecoveryPassword.recovery("email", "test@test.ru");
-        if (typeof (this.code) == "number" ||
-            toString(this.code).length == 5) {
-            this.RecoveryPassword_email = true;
-            this.passedTestCount++;
-            console.log("RecoveryPassword_sms_email => PASSED");
-        }
-    },
-
-    test_RecoveryPassword_validate() {
-        var check = RecoveryPassword.validate(this.code);
-        if (check == true) {
-            this.RecoveryPassword_validate = true;
-            this.passedTestCount++;
-            console.log("RecoveryPassword_email_Validate => PASSED");
-        } else console.log("validate_state: ", check);
-    },
-
-    test_all() {
-        this.test_codeGen_generate();
-        this.test_smsSend();
-        this.test_emailSend();
-        this.test_RecoveryPassword_sms();
-        this.test_RecoveryPassword_email();
-        this.test_RecoveryPassword_validate();
-
-        if (this.passedTestCount != 6) {
-            if (this.RecoveryPassword_sms == false)
-                console.log("RecoveryPassword_sms got a problem");
-            if (this.RecoveryPassword_email == false)
-                console.log("RecoveryPassword_email got a problem");
-            if (this.RecoveryPassword_validate == false)
-                console.log("RecoveryPassword_validate got a problem");
-            if (this.emailSend == false)
-                console.log("emailSend got a problem");
-            if (this.smsSend == false)
-                console.log("smsSend got a problem");
-            if (this.codeGen_generate == false)
-                console.log("codeGen_generate got a problem");
-
-            return console.log("passed tests count = ", this.passedTestCount);
-        }
-    }
-}
+    it("Validate code method", function() {
+        assert.equal(resultValidate, false);
+    });
+  });
