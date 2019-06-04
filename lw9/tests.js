94f87ae96f44f4282a1de8bcceee9f0315d6307e
Irnil <32452526+Irnil@users.noreply.github.com>
2019-06-04 11:28:23 +0300

    New tests added

--- lw9/tests.js
+++ lw9/tests.js
@@ -11,6 +11,22 @@ describe("RecoveryPassword", function () {
   it("Validate code method", function () {
     assert.equal(resultValidate, false);
   });
+
+  it("SMS recovery method wrong number", function () {
+    assert.equal(falseSMS, false);
+  });
+
+  it("Email recovery method wrong adress", function () {
+    assert.equal(falseEmail, false);
+  });
+
+  it("Wrong recovery method", function () {
+    assert.equal(wrongMethod, false);
+  });
+
+  it("Wrong data validation", function () {
+    assert.equal(wrongRecData, false);
+  });
 });
 
 describe("Code generator", function () {
@@ -18,3 +34,5 @@ describe("Code generator", function () {
     assert.equal(toString(cGcode).length, 18);
   });
 });
+
+
