a351480d360bbeed03b2dc1bcf4386168c442630
Irnil <32452526+Irnil@users.noreply.github.com>
2019-06-17 01:13:27 +0300

    GET one by PK added

--- lw8/payment/views.py
+++ lw8/payment/views.py
@@ -13,23 +13,29 @@ def get_delete_update_payments(request, pk):
         payment = Payment.objects.get(pk=pk)
     except Payment.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
     # get details of a single payment
     if request.method == 'GET':
-        return Response({})
+        serializer = PaymentSerializer(payment)
+        return Response(serializer.data)
+
     # delete a single payment
     elif request.method == 'DELETE':
         return Response({})
+
     # update details of a single payment
     elif request.method == 'PUT':
         return Response({})
 
 @api_view(['GET', 'POST'])
 def get_post_payments(request):
+    
     # get all payment
     if request.method == 'GET':
         payments = Payment.objects.all()
         serializer = PaymentSerializer(payments, many=True)
         return Response(serializer.data)
+
     # insert a new record for a payment
     elif request.method == 'POST':
         return Response({})
