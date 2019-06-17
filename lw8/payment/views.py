f51cd0a2dcba0e8d2f88b3afcf52050ab6dd1390
Irnil <32452526+Irnil@users.noreply.github.com>
2019-06-17 15:02:04 +0300

    DELETE added. CRUD is full

--- lw8/payment/views.py
+++ lw8/payment/views.py
@@ -28,8 +28,9 @@ def get_delete_update_payments(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # delete a single payment
-    elif request.method == 'DELETE':
-        return Response({})    
+    if request.method == 'DELETE':
+        payment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  
 
 @api_view(['GET', 'POST'])
 def get_post_payments(request):
